b9e2d27685dd0360e81ac23630bb81418ef2ccdc
Ralf303 <nikitasuhanov34@gmail.com>
2024-12-15 18:12:11 +0300

    add bot

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
+import { config } from "dotenv";
 import express from "express";
 import fs from "fs";
+import { Telegraf } from "telegraf";
+
+config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 export const start = async () => {
   const app = express();
 
   app.use(express.json());
 
-  app.post("/", (req, res) => {
+  app.post("/", async (req, res) => {
     console.log(req);
 
     const data = JSON.stringify(req.body, null, 2);
-    fs.writeFile(`request_${Date.now()}.txt`, data, (err) => {
+    const fileName = `request_${Date.now()}.txt`;
+
+    fs.writeFile(fileName, data, async (err) => {
       if (err) {
         return res.status(500).send("Error writing file");
       }
-      res.send("Data saved!");
+
+      // Отправка файла через бот
+      await bot.telegram.sendDocument("1157591765", {
+        source: fileName,
+        filename: fileName,
+      });
+
+      res.send("Data saved and file sent!");
     });
   });
 
@@ -22,7 +37,8 @@ export const start = async () => {
     res.send("Hello World!");
   });
 
-  app.listen(3000, () => {
+  app.listen(3000, async () => {
     console.log("Server is running on port 3000");
+    bot.telegram.sendMessage("1157591765", "Парсер запущен");
   });
 };
