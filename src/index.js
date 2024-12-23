c11b4f679ffb55858dd08ac32b7cfe92ad76c7c7
Ralf303 <nikitasuhanov34@gmail.com>
2024-12-23 07:17:57 +0300

    debug

--- src/index.js
+++ src/index.js
@@ -35,12 +35,16 @@ export const start = async () => {
       const hour = now.getHours();
       const isWeekday = dayOfWeek >= 1 && dayOfWeek <= 5;
       const isWithinTimeFrame = hour >= 6 && hour <= 22;
+      console.log(signal);
 
       if (signal && isWeekday && isWithinTimeFrame) {
         const text = `НОВЫЙ СИГНАЛ:\n\n${signal}`;
         await bot.telegram.sendMessage("1157591765", JSON.stringify(data));
 
         const users = await getVerifieUsers();
+        console.log(users.length);
+        console.log(text);
+
         for (const user of users) {
           try {
             await ctx.telegram.sendMessage(user.chatId, text);
@@ -52,7 +56,7 @@ export const start = async () => {
         }
       }
 
-      if (result) {
+      if (data) {
         res.send("Data received and processed!");
       } else {
         res.status(500).send("Error processing data");
